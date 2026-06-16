145facda5b4f2a6475a8da637abf0e18f89cb63a
Stewy-oso <118993775+Stewy-oso@users.noreply.github.com>
2026-06-16 18:39:36 +0100

    Delete Alters.sql

--- Alters.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE Customers
-MODIFY name VARCHAR2(35);
-
-ALTER TABLE Customers
-MODIFY email VARCHAR2(50); -- Even though the lower standard is 63
-
-ALTER TABLE Customers
-MODIFY PhoneNo VARCHAR2(12);
