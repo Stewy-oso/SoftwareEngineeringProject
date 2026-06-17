f50e9cb20f9384d4e7fb9ed0521e966c46480337
Stewy-oso <118993775+Stewy-oso@users.noreply.github.com>
2026-06-17 18:27:11 +0100

    DB changes

--- Database/Alters.sql
+++ Database/Alters.sql
@@ -5,9 +5,6 @@ ALTER TABLE Customers
 MODIFY email VARCHAR2(50); -- Even though the lower standard is 63
 
 ALTER TABLE Customers
-MODIFY PhoneNo VARCHAR2(12);
-
-ALTER TABLE Customers
 ADD CONSTRAINT PhoneNo UNIQUE(PhoneNo);
 
 ALTER TABLE Customers
@@ -41,3 +38,40 @@ WHERE statusID IS NULL;
 
 ALTER TABLE Customers
 MODIFY statusID NOT NULL;
+
+-- realised '+' numbers can be 13 characters long, I.E +353xxxxxxxxx
+ALTER TABLE Customers
+MODIFY PhoneNo VARCHAR2(13);
+
+-- Decided to split name into forename and surname
+ALTER TABLE Customers
+RENAME COLUMN Name TO Forename;
+
+ALTER TABLE Customers
+ADD Surname VARCHAR2(35);
+
+UPDATE Customers
+SET Surname = 'test'
+WHERE Surname IS NULL;
+
+ALTER TABLE Customers
+MODIFY Surname NOT NULL;
+
+ALTER TABLE Customers
+RENAME CONSTRAINT SYS_C007564 TO NN_Forename;
+
+ALTER TABLE Customers
+RENAME CONSTRAINT SYS_C007565 TO NN_Email;
+
+ALTER TABLE Customers
+RENAME CONSTRAINT SYS_C007652 TO NN_StatusID;
+
+ALTER TABLE Customers
+RENAME CONSTRAINT SYS_C007653 TO NN_Surname;
+
+SELECT constraint_name, table_name
+FROM user_constraints
+WHERE constraint_name = 'SYS_C007563';
+
+ALTER TABLE Customers
+RENAME CONSTRAINT SYS_C007563 TO PK_Customers;
